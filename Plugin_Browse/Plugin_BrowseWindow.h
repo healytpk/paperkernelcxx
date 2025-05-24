115bfc905115bdf291d053f6daa39f8a7bbca3fa
healytpk <thomaspkhealy@yahoo.com>
2025-05-24 12:48:03 +0100

    Change name of preprocessor macro SAMPLEGUIPLUGINWINDOW1

--- Plugin_Browse/Plugin_BrowseWindow.h
+++ Plugin_Browse/Plugin_BrowseWindow.h
@@ -1,18 +1,18 @@
-#ifndef _SAMPLEGUIPLUGINWINDOW1_H_
-#define _SAMPLEGUIPLUGINWINDOW1_H_
+#ifndef _PLUGIN_BROWSEWINDOW_H_
+#define _PLUGIN_BROWSEWINDOW_H_
 
 #include <wx/textctrl.h>
 
 #include <wxGuiPluginWindowBase.h>
 
-#define ID_SAMPLEGUIPLUGINWINDOW1 10000
+#define ID_PLUGIN_BROWSEWINDOW 10000
 #define ID_SAMPLE_TEXTCTRL 10001
 #define ID_SEND_EVENT_BUTTON 10002
-#define SYMBOL_SAMPLEGUIPLUGINWINDOW1_STYLE wxTAB_TRAVERSAL
-#define SYMBOL_SAMPLEGUIPLUGINWINDOW1_TITLE _("Plugin_BrowseWindow")
-#define SYMBOL_SAMPLEGUIPLUGINWINDOW1_IDNAME ID_SAMPLEGUIPLUGINWINDOW1
-#define SYMBOL_SAMPLEGUIPLUGINWINDOW1_SIZE wxSize(400, 300)
-#define SYMBOL_SAMPLEGUIPLUGINWINDOW1_POSITION wxDefaultPosition
+#define SYMBOL_PLUGIN_BROWSEWINDOW_STYLE wxTAB_TRAVERSAL
+#define SYMBOL_PLUGIN_BROWSEWINDOW_TITLE _("Plugin_BrowseWindow")
+#define SYMBOL_PLUGIN_BROWSEWINDOW_IDNAME ID_PLUGIN_BROWSEWINDOW
+#define SYMBOL_PLUGIN_BROWSEWINDOW_SIZE wxSize(400, 300)
+#define SYMBOL_PLUGIN_BROWSEWINDOW_POSITION wxDefaultPosition
 
 class Plugin_BrowseWindow : public wxGuiPluginWindowBase {
 
@@ -24,9 +24,9 @@ class Plugin_BrowseWindow : public wxGuiPluginWindowBase {
 public:
 
     Plugin_BrowseWindow();
-    Plugin_BrowseWindow(wxGuiPluginBase * plugin, wxWindow* parent, wxWindowID id = SYMBOL_SAMPLEGUIPLUGINWINDOW1_IDNAME, const wxPoint& pos = SYMBOL_SAMPLEGUIPLUGINWINDOW1_POSITION, const wxSize& size = SYMBOL_SAMPLEGUIPLUGINWINDOW1_SIZE, long style = SYMBOL_SAMPLEGUIPLUGINWINDOW1_STYLE );
+    Plugin_BrowseWindow(wxGuiPluginBase * plugin, wxWindow* parent, wxWindowID id = SYMBOL_PLUGIN_BROWSEWINDOW_IDNAME, const wxPoint& pos = SYMBOL_PLUGIN_BROWSEWINDOW_POSITION, const wxSize& size = SYMBOL_PLUGIN_BROWSEWINDOW_SIZE, long style = SYMBOL_PLUGIN_BROWSEWINDOW_STYLE );
 
-    bool Create(wxGuiPluginBase * plugin, wxWindow* parent, wxWindowID id = SYMBOL_SAMPLEGUIPLUGINWINDOW1_IDNAME, const wxPoint& pos = SYMBOL_SAMPLEGUIPLUGINWINDOW1_POSITION, const wxSize& size = SYMBOL_SAMPLEGUIPLUGINWINDOW1_SIZE, long style = SYMBOL_SAMPLEGUIPLUGINWINDOW1_STYLE );
+    bool Create(wxGuiPluginBase * plugin, wxWindow* parent, wxWindowID id = SYMBOL_PLUGIN_BROWSEWINDOW_IDNAME, const wxPoint& pos = SYMBOL_PLUGIN_BROWSEWINDOW_POSITION, const wxSize& size = SYMBOL_PLUGIN_BROWSEWINDOW_SIZE, long style = SYMBOL_PLUGIN_BROWSEWINDOW_STYLE );
 
     ~Plugin_BrowseWindow();
 
