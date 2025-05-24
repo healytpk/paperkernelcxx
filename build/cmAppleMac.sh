24cd6933f0cb272d52341ae7d69e43fe01397876
healytpk <thomaspkhealy@yahoo.com>
2025-05-24 18:09:06 +0100

    Seventh attempt to link archive with xapian

--- build/cmAppleMac.sh
+++ build/cmAppleMac.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ -e "../../all_cxx_papers.tar.xz" ]; then
+if [ -e "../all_cxx_papers.tar.xz" ]; then
     echo Compressed tar archive of all C++ papers already exists
 else
     echo Downloading compressed tar archive of all C++ papers from virjacode.com
-    curl http://virjacode.com/downloads/all_cxx_papers.tar.xz -o ../../all_cxx_papers.tar.xz
+    curl http://virjacode.com/downloads/all_cxx_papers.tar.xz -o ../all_cxx_papers.tar.xz
 fi
 
 rm -dr Mac
