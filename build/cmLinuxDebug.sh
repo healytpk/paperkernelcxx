36dbe5473280af48132d329dcd5b4eb65aea17ad
healytpk <thomaspkhealy@yahoo.com>
2025-05-26 10:58:33 +0100

    Only build Xapian in Debug mode for Debug build type

--- build/cmLinuxDebug.sh
+++ build/cmLinuxDebug.sh
@@ -8,15 +8,16 @@ else
     curl -o ../all_cxx_papers.tar.zst http://www.virjacode.com/downloads/all_cxx_papers.tar.zst
     if [ ! -e "../all_cxx_papers.tar.zst" ]; then
         echo "Failed to retrieve all C++ papers from the internet."
-        exit
+        exit 1
     fi
 fi
 
 if [ ! -e "../xapian-core-1.4.29.tar.xz" ]; then
+    echo Downloading libxapian from oligarchy.co.uk
     curl -o ../xapian-core-1.4.29.tar.xz https://oligarchy.co.uk/xapian/1.4.29/xapian-core-1.4.29.tar.xz
     if [ ! -e "../xapian-core-1.4.29.tar.xz" ]; then
         echo "Failed to retrieve libxapian source code from the internet."
-        exit
+        exit 1
     fi
 fi
 
@@ -28,12 +29,6 @@ if [ ! -d "./xapian" ]; then
     mkdir -p build_debug
     cd build_debug
     ../configure --enable-static=yes --enable-shared=no --enable-assertions --enable-log CPPFLAGS="-D_GLIBCXX_DEBUG" CXXFLAGS="-ggdb3"
-    make -j `nproc`
-
-    cd ../
-    mkdir -p build_release
-    cd build_release
-    ../configure --enable-static=yes --enable-shared=no CPPFLAGS="-DNDEBUG" CXXFLAGS="-O3"
     make -j `nproc`
 fi
 
