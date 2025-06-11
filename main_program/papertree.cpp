e69a9e4ffcca12eb80df2433e8c7f51ecb610489
healytpk <thomaspkhealy@yahoo.com>
2025-06-11 16:20:47 +0100

    Rename paper_tree_contents to AUTO_GENERATED_tree_contents_paper

--- main_program/papertree.cpp
+++ main_program/papertree.cpp
@@ -1,4 +1,4 @@
 #include "papertree.hpp"
 
 std::map< unsigned, std::vector< std::tuple<unsigned, char const*, char const* > > > g_map_papers =
-#    include "paper_tree_contents.hpp"
+#    include "AUTO_GENERATED_tree_contents_paper.hpp"
