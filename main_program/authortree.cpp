9bb27b8b3b431e6c4ba345fe2a6ab14429e167ec
healytpk <thomaspkhealy@yahoo.com>
2025-06-26 13:46:47 +0100

    authortree and papertree switch on wxUSE_UNICODE_WCHAR

--- main_program/authortree.cpp
+++ main_program/authortree.cpp
@@ -1,5 +1,11 @@
 #include "authortree.hpp"
 
 extern std::pair< long long unsigned, std::pair< wxString, std::vector<Paper> > > const g_map_authors[216u] =
-#    include "AUTO_GENERATED_tree_contents_author.hpp"
+
+#if wxUSE_UNICODE_WCHAR
+#    include "AUTO_GENERATED_tree_contents_author_wchar_t.hpp"
+#else
+#    include "AUTO_GENERATED_tree_contents_author_char.hpp"
+#endif
+
 ;
--- main_program/papertree.cpp
+++ main_program/papertree.cpp
@@ -6,6 +6,10 @@ extern std::pair<unsigned, std::vector< std::tuple<unsigned, wxStringCharType co
 decltype(g_map_papers) g_map_papers =
 #endif
 
-#    include "AUTO_GENERATED_tree_contents_paper.hpp"
+#if wxUSE_UNICODE_WCHAR
+#    include "AUTO_GENERATED_tree_contents_paper_wchar_t.hpp"
+#else
+#    include "AUTO_GENERATED_tree_contents_paper_char.hpp"
+#endif
 
 ;
