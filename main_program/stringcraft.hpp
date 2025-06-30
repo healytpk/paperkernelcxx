34996a4e95439838e43a645468f7331234f1afe8
healytpk <thomaspkhealy@yahoo.com>
2025-06-30 14:44:13 +0100

    stringcraft now has StringLiteral and Hash

--- main_program/stringcraft.hpp
+++ main_program/stringcraft.hpp
@@ -7,7 +7,8 @@ wxString without copying the original string (i.e. no dynamic memory
 allocation required).
 */
 
-#include <cstddef>         // ptrdiff_t size_t
+#include <cstddef>         // ptrdiff_t, size_t
+#include <cstdint>         // uint_fast64_t
 #include <algorithm>       // max
 #include <string>          // basic_string, string, wstring
 #include <type_traits>     // is_reference, is_same
@@ -20,6 +21,67 @@ allocation required).
     static_assert( std::is_same_v< char  , wxStringCharType> );
     typedef std::string wxstring;
 #endif
+
+template<std::size_t N>
+struct StringLiteral {
+    static constexpr std::size_t len = N;
+    wxStringCharType value[N];
+    consteval StringLiteral(wxStringCharType const (&str)[N]) noexcept
+    {
+        for ( std::size_t i = 0; i < N; ++i ) value[i] = str[i];
+    }
+    consteval wxStringCharType &operator[](std::size_t const arg) noexcept
+    {
+        return value[arg];
+    }
+    consteval wxStringCharType const &operator[](std::size_t const arg) const noexcept
+    {
+        return value[arg];
+    }
+};
+
+consteval wxStringCharType tolower_consteval(wxStringCharType const c) noexcept
+{
+    switch ( c )
+    {
+        case wxS('A'): return wxS('a'); case wxS('B'): return wxS('b'); case wxS('C'): return wxS('c'); case wxS('D'): return wxS('d');
+        case wxS('E'): return wxS('e'); case wxS('F'): return wxS('f'); case wxS('G'): return wxS('g'); case wxS('H'): return wxS('h');
+        case wxS('I'): return wxS('i'); case wxS('J'): return wxS('j'); case wxS('K'): return wxS('k'); case wxS('L'): return wxS('l');
+        case wxS('M'): return wxS('m'); case wxS('N'): return wxS('n'); case wxS('O'): return wxS('o'); case wxS('P'): return wxS('p');
+        case wxS('Q'): return wxS('q'); case wxS('R'): return wxS('r'); case wxS('S'): return wxS('s'); case wxS('T'): return wxS('t');
+        case wxS('U'): return wxS('u'); case wxS('V'): return wxS('v'); case wxS('W'): return wxS('w'); case wxS('X'): return wxS('x');
+        case wxS('Y'): return wxS('y'); case wxS('Z'): return wxS('z');
+    }
+
+    return c;
+}
+
+template<StringLiteral input>
+consteval std::uint_fast64_t Hash(void)
+{
+    std::uint_fast64_t h = 0xcbf29ce484222325;
+
+    for ( std::size_t i = 0u; i < decltype(input)::len; ++i )
+    {
+        wxStringCharType const c = tolower_consteval(input[i]);
+        bool is_letter = false;
+        for ( wxStringCharType const letter : wxS("abcdefghijklmnopqrstuvwxyz") )
+        {
+            if ( letter == c )
+            {
+                is_letter = true;
+                break;
+            }
+        }
+        if ( false == is_letter ) continue;
+        h ^= c;
+        h *= 0x100000001B3;
+    }
+
+    return h;
+}
+
+static_assert( Hash<wxS("Thomas PK Healy")>() == Hash<wxS("thomas p. k. healy")>() );
 
 #ifdef PAPERKERNEL_DONT_USE_STRING_PRETENDER
 
