efb4d3f5826e5b131529966e5e0565abc86e1c92
healytpk <t@virjacode.com>
2025-06-30 11:29:53 +0100

    Now has PAPERKERNEL_DONT_USE_PRETENDER [skip actions]

--- main_program/stringcraft.hpp
+++ main_program/stringcraft.hpp
@@ -7,7 +7,8 @@ wxString without copying the original string (i.e. no dynamic memory
 allocation required).
 */
 
-#include <cstddef>         // size_t
+#include <cstddef>         // ptrdiff_t size_t
+#include <algorithm>       // max
 #include <string>          // basic_string, string, wstring
 #include <type_traits>     // is_reference, is_same
 #include <wx/string.h>     // wxString, wxStringCharType
@@ -20,19 +21,35 @@ allocation required).
     typedef std::string wxstring;
 #endif
 
+#ifdef PAPERKERNEL_DONT_USE_STRING_PRETENDER
+
 // The next function is for getting an std::basic_string
 // from a wxString without copying anything
 inline wxstring const &wxString_inner(wxString const &s)
 {
+#if defined(_MSC_VER) && defined(_DEBUG) && !defined(NDEBUG)
+    constexpr std::ptrdiff_t offset = 8u;
+#else
+    constexpr std::ptrdiff_t offset = 0u;
+#endif
+
     // We don't need the complication of calling ToStdString or ToStdWstring,
-    // we can just assume that the std::basic_string is at offset 0 inside
+    // we can just assume that the std::basic_string is at offset 0 or 8 inside
     // the wxString object
-    return *static_cast<wxstring const*>(static_cast<void const*>(&s));
+    return
+      *static_cast<wxstring const*>(
+         static_cast<void const*>(
+           static_cast<char const*>(
+             static_cast<void const*>(&s) ) + offset ));
 }
 
 // The next POD struct mimics the layout of std::basic_string so that
 // it can be configured to refer to any string without copying the string
 struct Pretender_wxstring {
+
+#if defined(_MSC_VER) && defined(_DEBUG) && !defined(NDEBUG)
+    void *_Myproxy;    // Iterator debugging proxy (Debug mode only)
+#endif
 
     // This layout works for libstdc++ and also the Microsoft compiler
     wxStringCharType const *p;
@@ -40,7 +57,11 @@ struct Pretender_wxstring {
 
     template<std::size_t bytes>
     constexpr Pretender_wxstring(wxStringCharType const (&arg)[bytes]) noexcept
-      : p(arg), len(bytes - 1u), allocd(bytes - 1u) {}
+        :
+#if defined(_MSC_VER) && defined(_DEBUG) && !defined(NDEBUG)
+        _Myproxy(nullptr),    // Iterator debugging proxy (Debug mode only)
+#endif
+      p(arg), len(bytes - 1u), allocd( std::max(32uz, bytes - 1uz) ) {}
 
     constexpr operator wxstring const&(void) const noexcept
     {
@@ -54,5 +75,37 @@ struct Pretender_wxstring {
 };
 
 typedef Pretender_wxstring Pretender_wxString;
+
+#else
+
+// The next function is for getting an std::basic_string
+// from a wxString without copying anything
+inline wxstring const &wxString_inner(wxString const &s)
+{
+#if wxUSE_UNICODE_WCHAR
+    static_assert( std::is_reference_v< decltype(s.ToStdWstring()) > );
+    return s.ToStdWstring();
+#else
+    static_assert( std::is_reference_v< decltype(s.ToStdString()) > );
+    return s.ToStdString();
+#endif
+}
+
+struct Pretender_wxString {
+
+    wxString wxs;
+
+    template<std::size_t bytes>
+    constexpr Pretender_wxString(wxStringCharType const (&arg)[bytes]) noexcept
+        : wxs(arg) {}
+
+    operator wxString const&(void) const noexcept
+    {
+        return wxs;
+    }
+};
+
+
+#endif
 
 #endif    // ifndef INCLUSION_GUARD
