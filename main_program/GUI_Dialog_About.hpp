99db39b3332d2665a4805a719c6db083431c52e3
healytpk <thomaspkhealy@yahoo.com>
2025-07-14 23:25:14 +0100

    At runtime, multiply About Box dimensions by constant for platform [skip actions]

--- main_program/GUI_Dialog_About.hpp
+++ main_program/GUI_Dialog_About.hpp
@@ -6,7 +6,16 @@
 class Dialog_About : public Dialog_About__Auto_Base_Class {
 public:
 	Dialog_About(wxWindow *const parent)
-	  : Dialog_About__Auto_Base_Class(parent) {}
+	  : Dialog_About__Auto_Base_Class(parent)
+    {
+#ifdef __WXGTK__
+        wxSize s = this->GetSize();
+        s.x = (int)((s.x * 1.1L) + 1.0L);
+        s.y = (int)((s.y * 1.1L) + 1.0L);
+        this->SetSize(s);
+        this->Layout();
+#endif
+    }
 
 	void dlgAbout_OnClose(wxCloseEvent&) override
 	{
