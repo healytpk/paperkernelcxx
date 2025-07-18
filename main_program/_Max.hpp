431dc2872bd5427f5a8c4f02ddd7873c9d8c4e8d
T. P. K. Healy <57729245+healytpk@users.noreply.github.com>
2025-07-18 19:02:35 +0100

    Purely cosmetic _Max.hpp [skip actions]

--- main_program/_Max.hpp
+++ main_program/_Max.hpp
@@ -2,7 +2,7 @@
 #define HEADER_INCLUSION_GUARD_fbde6307_28ac_4082_904d_2984d8802179
 
 #include <limits>                       // numeric_limits<T>::max()
-#include <type_traits>                  // is_integral_v
+#include <type_traits>                  // is_integral, is_unsigned
 
 struct _Max_t {
     template<typename T> requires std::is_integral_v<T> && std::is_unsigned_v<T>
